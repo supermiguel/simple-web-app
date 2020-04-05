f1de0c198fafa406e2cf73f0c7cc070d27f52650
jx-auditsource-dev <miguel.veliz@schellman.com>
2020-04-04 20:49:26 -0400

    favorite fruit

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,11 @@ FROM python:3
 
 WORKDIR /usr/src/app
 
+ENV FRUIT
+
 COPY requirements.txt ./
 RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
-CMD [ "python", "./app.py" ]
+CMD [ "python", "./app.py" "$FRUIT" ]
--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+import sys
 from flask import Flask
 app = Flask(__name__)
 
+#print(sys.argv[1])
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return "Hello World! my favorite fruit is: " + (sys.argv[1])
 
 if __name__ == '__main__':
     app.run()
